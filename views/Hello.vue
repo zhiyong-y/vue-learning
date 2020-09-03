9df9f3a8c2a1dbc1aee072ebeb21c78e862b0414
yuzhiyong <15684187702@163.com>
2020-09-03 22:36:13 +0800

    a

---
+++ views/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        mounted () {
+            
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        watch: {
+            data(newValue, oldValue) {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
